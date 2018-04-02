2e05fec578d1907928fc7172b007d40b5381df7f
raoyach <yach.avinash@gmail.com>
2018-04-02 03:34:00 -0700

    Test Comment and update .gitignore file.

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -14,7 +14,7 @@ import java.util.Map;
  */
 public class Main {
     private static StockList stockList = new StockList();
-
+    // Project's entry point.
     public static void main(String[] args) {
 	    StockItem temp = new StockItem("bread", 0.86, 100);
         stockList.addStock(temp);
